fa18207ebfdeab7af5418134ec1f8d0d9aa7fd97
Alvin Dizon <alvin.dizon91@gmail.com>
2022-01-24 13:58:50 +0800

    [TestingCodelab] Part 5: merged and unmerged semantics trees
    Signed-off-by: Alvin Dizon <alvin.dizon91@gmail.com>

--- TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -1,8 +1,9 @@
 package com.example.compose.rally
 
-import androidx.compose.ui.test.assertIsSelected
+import androidx.compose.ui.test.hasContentDescription
+import androidx.compose.ui.test.hasParent
+import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onRoot
 import androidx.compose.ui.test.printToLog
 import com.example.compose.rally.ui.components.RallyTopAppBar
@@ -28,10 +29,16 @@ class TopAppBarTest {
             }
         }
 
-        composeTestRule.onRoot().printToLog("currentLabelExists")
+        composeTestRule.onRoot(useUnmergedTree = true).printToLog("currentLabelExists")
 
         composeTestRule
-            .onNodeWithContentDescription(RallyScreen.Accounts.name)
-            .assertIsSelected()
+            .onNode(
+        hasText(RallyScreen.Accounts.name.uppercase()) and
+                hasParent(
+                    hasContentDescription(RallyScreen.Accounts.name)
+                ),
+                useUnmergedTree = true
+            )
+            .assertExists()
     }
 }
